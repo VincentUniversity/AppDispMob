4e7d3fafce4119d13a106444526c257aa715ec72
VincentUniversity <81414879+VincentUniversity@users.noreply.github.com>
2022-02-16 09:51:32 +0100

    Update auth.service.ts

--- codice/src/app/services/auth.service.ts
+++ codice/src/app/services/auth.service.ts
@@ -1,31 +1,49 @@
 import { Injectable } from '@angular/core';
-import firebase from 'firebase/app';
-import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import {AngularFireAuth} from '@angular/fire/auth'
+
+export  interface UserPro{
+  username: string;
+  uid: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { 
+  private user : UserPro;
 
-  }
+  constructor(public auth: AngularFireAuth) { }
+
 
   loginFireauth(value){
-    return new Promise<any> ((resolve, reject)=>{
-      firebase.auth().signInWithEmailAndPassword(value.email, value.password).then(
+   return new Promise<any> ( (resolve, reject)=>{
+     firebase.auth().signInWithEmailAndPassword(value.email, value.password).then(
+       res => resolve(res),
+       error => reject(error)
+     )
+   })
+  }
+
+
+  setUser(user: UserPro){
+    return this.user = user;
+  }
+
+  getUid(): string{
+    return this.user.uid;
+  }
+
+
+
+  userRegistration(value){
+    return new Promise<any> ( (resolve, reject)=>{
+      firebase.auth().createUserWithEmailAndPassword(value.email,value.password).then(
         res => resolve(res),
         error => reject(error)
       )
     })
   }
 
-  userRegistration(value){
-    return new Promise<any>((resolve,reject)=>{
-      firebase.auth().createUserWithEmailAndPassword(value.email, value.password).then(
-        res => resolve(res),
-        error => reject(error)
-      )
-    })
-  }
 }
